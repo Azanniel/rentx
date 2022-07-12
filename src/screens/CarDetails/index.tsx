7d71580925fad5ed7b808a5a69a1cfc833b4ca6a
azanniel <leo.azannielttt@gmail.com>
2022-07-12 13:47:53 -0400

    add details of the car

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,19 @@
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
 import {
   Container,
-  Header
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles';
 
 export function CarDetails() {
@@ -11,6 +23,29 @@ export function CarDetails() {
       <Header>
         <BackButton />
       </Header>
+
+      <CarImages>
+        <ImageSlider imageUrl={['https://www.pngmart.com/files/10/White-Audi-PNG-Transparent-Image.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado 
+          na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
 
     </Container>
   );
